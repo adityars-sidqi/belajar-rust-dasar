77138a7f58c2ee7d0634837b9dd2c46ba94e2ad1
Aditya Rahman Sidqi <adityars.sidqi@gmail.com>
2025-08-08 04:45:50 +0700

    belajar-rust-dasar: variable

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("Hello, world!");
+
+    println!("Hello, Aditya!");
+}
+
+#[test]
+fn hello_test() {
+    println!("Hello, Test!");
+}
+
+#[test]
+fn test_variable() {
+    let name = "Aditya";
+    println!("Hello, {}!", name);
+}
