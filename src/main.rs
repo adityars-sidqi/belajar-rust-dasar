108542b01611fb1dca08074ebd1658b964f6f588
Aditya Rahman Sidqi <adityars.sidqi@gmail.com>
2025-08-08 04:53:00 +0700

    belajar-rust-dasar: mutable variable

--- src/main.rs
+++ src/main.rs
@@ -14,3 +14,12 @@ fn test_variable() {
     let name = "Aditya";
     println!("Hello, {}!", name);
 }
+
+#[test]
+fn test_mutable() {
+    let mut name = "Aditya";
+    println!("Hello, {}!", name);
+
+    name = "Rahman";
+    println!("Hello, {}!", name);
+}
